1c90bb3681a1bb9c905bf629c694be588db06211
AdarshM18 <mishraadarsh903@gmail.com>
2025-01-26 14:29:50 +0530

    Your commit message here

--- myapp/vite-project/src/App.jsx
+++ myapp/vite-project/src/App.jsx
@@ -2,9 +2,12 @@ import Chai from "./Chai"
 
 function App() {
 
+  const username = "Chai and Coffee!"
+
   return (
     <>
     <Chai/>
+    <h1>I want {username}</h1>
     <h1>Mishra</h1>
     </>
   )
